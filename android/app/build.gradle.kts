054432a821ab22633a490ce11467eb50260ef261
Myke Kolawole <105569489+Mykol5@users.noreply.github.com>
2026-01-26 00:13:41 +0100

    Update build.gradle.kts

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,41 +1,36 @@
-plugins {
-    id("com.android.application")
-    id("kotlin-android")
-    id("dev.flutter.flutter-gradle-plugin")
-}
+apply plugin: 'com.android.application'
+apply plugin: 'kotlin-android'
+apply plugin: 'dev.flutter.flutter-gradle-plugin'
 
 android {
-    namespace = "com.example.food_sharing_app"
-    compileSdk = 33
+    namespace 'com.example.food_sharing_app'
+    compileSdkVersion 34 // or use flutter.compileSdkVersion
+
+    defaultConfig {
+        applicationId "com.example.food_sharing_app"
+        minSdkVersion 21 // or flutter.minSdkVersion
+        targetSdkVersion 34 // or flutter.targetSdkVersion
+        versionCode 1
+        versionName "1.0"
+    }
 
     compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility JavaVersion.VERSION_17
+        targetCompatibility JavaVersion.VERSION_17
     }
 
     kotlinOptions {
-        jvmTarget = JavaVersion.VERSION_17.toString()
-    }
-
-    defaultConfig {
-        applicationId = "com.example.food_sharing_app"
-        minSdk = 21
-        targetSdk = 33
-        versionCode = 1
-        versionName = "1.0"
+        jvmTarget = '17'
     }
 
     buildTypes {
         release {
-            // For CI builds, no signing
-            // Remove the debug signing config for release builds
-            // signingConfig = signingConfigs.getByName("debug")
-            isMinifyEnabled = false
-            isShrinkResources = false
+            signingConfig signingConfigs.debug
+            minifyEnabled false
         }
     }
 }
 
 flutter {
-    source = "."
+    source '../..'
 }
